e275c14640174746c42247bc69f6598c7c029fe1
Hannah Riley <riley.hannahm@gmail.com>
2020-02-12 16:43:28 -0800

    made new branch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header.js';
+
+export default class App extends Component {
+  
+  render() {
+  return (
+   <Header 
+     title="Pokedex!"   />
+  );
+}}
+
+
