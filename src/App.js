52fae01c9c7836350fb14921159d1fa5b968879a
Hannah Riley <riley.hannahm@gmail.com>
2020-02-13 11:38:45 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js';
+import pokeData from './data.js'
+import ImageList from './ImageList.js'
+import request from 'superagent';
 
 export default class App extends Component {
   
+state = {
+  pokeApi: [],
+}
+
+
+async componentDidMount() {
+  const pokeApiData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex`);
+
+  const pokeApi = this.state.pokeApi;
+  this.setState({ pokeApi: pokeApiData.body.results })
+  
+  console.log(pokeApiData.body.results);
+};
+
+
   render() {
   return (
+   <>
    <Header 
      title="Pokedex!"   />
+
+ <ImageList pokemonData = {this.state.pokeApi} />
+
+ 
+
+ </>
   );
 }}
 
